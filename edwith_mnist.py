203564d9b21bd667b7b8c339dd39f9c1b3d73439
yoon <yoonakim0808@gmail.com>
2021-03-08 23:01:54 +0900

    정리

---
+++ edwith_mnist.py
@@ -0,0 +1,7 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from scipy.misc import imread
+#from tensorflow.examples.tutorials.mnist import input_data
+
+#print("TENSORFLOW VERSION IS [%s]" %(tf.__version__))
